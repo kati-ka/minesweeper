8409b4470ab6e6f5d48e099fab654f612c6262a4
Katarína Grešová <gresova11@gmail.com>
2017-09-14 10:39:20 +0200

    project part 2 - board as an array

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,21 @@
-const blankLine = '   |   |   ';
-const guessLine = ' 1 |   |   ';
-const bombLine =  '   | B |   ';
+const printBoard = board => {
+  console.log('Current Board: ');
 
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+}
 
-console.log('This is what a board with a guess and a bomb on it would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+// array representing game board
+let board = [];
+
+board.push([],[],[]);
+board[0] = [' ', ' ', ' '];
+board[1] = [' ', ' ', ' '];
+board[2] = [' ', ' ', ' '];
+
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board);
