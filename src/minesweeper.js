0a8316be2c735b4c530b844f927c7f5929259d6f
Katarína Grešová <gresova11@gmail.com>
2017-09-14 14:29:52 +0200

    project part 3 - dynamically generated boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,21 +1,49 @@
 const printBoard = board => {
-  console.log('Current Board: ');
-
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+  let formatedBoard = board.map(row => {
+    return row.join(' | '); // join items in every row
+  }).join('\n');            // join all rows - put new line between them
+  console.log(formatedBoard);
 }
 
-// array representing game board
-let board = [];
+const generatePlayesBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
+};
 
-board.push([],[],[]);
-board[0] = [' ', ' ', ' '];
-board[1] = [' ', ' ', ' '];
-board[2] = [' ', ' ', ' '];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (let columnIndex = 0; columnIndex < numberOfColumns; columnIndex++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
 
-printBoard(board);
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    if (board[randomRowIndex][randomColumnIndex] === null) {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
+  }
 
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+  return board;
+};
+
+let playerBoard = generatePlayesBoard(3,4);
+console.log('Player Board: ');
+printBoard(playerBoard);
+
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Bomb board: ');
+printBoard(bombBoard);
